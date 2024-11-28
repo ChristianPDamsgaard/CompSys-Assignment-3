5b64993fe5502f89cc52d5c8874e7b845d62644c
s235282 <s235282@dtu.dk>
2024-11-28 02:15:28 +0100

    Add files via upload

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -10,115 +10,121 @@ class Accelerator extends Module {
     val dataRead = Input(UInt (32.W))
     val writeEnable = Output(Bool ())
     val dataWrite = Output(UInt (32.W))
-
   })
-
-  val dataMemory = Module(new DataMemory())
 
   //State enum and register
   val idle :: loadVals :: border::write::inc :: done  :: self :: left :: right :: up :: down :: adhoc = Enum (12)
   val stateReg = RegInit(idle)
 
   //Support registers
-  val x = 0.U(32.W)
-  val y = 0.U(32.W)
+  val x = RegInit(0.U(32.W))
+  val y = RegInit(0.U(32.W))
 
   val addressReg = RegInit(0.U(16.W))
   val trueAddressReg = RegInit(0.U(16.W))
   val dataReg = RegInit(0.U(32.W))
 
   //Default values
-  dataMemory.io.writeEnable := false.B
-  dataMemory.io.dataWrite := dataReg
-  dataMemory.io.address := 0.U(16.W)
+  io.writeEnable := false.B
+  io.dataWrite := dataReg
+  io.address := 0.U(16.W)
   io.done := false.B
 
   //FSMD switch
   switch(stateReg) {
     is(idle) {
       when(io.start) {
-        stateReg := loadVals
         addressReg := RegInit(0.U(16.W))
+        stateReg := loadVals
       }
     }
     is(loadVals) {
       addressReg := (20.U * y) + x
+      io.address := ((20.U * y) + x)(15,0)
       stateReg := border
     }
 
     is(border) {
       when(x === 0.U || x === 19.U || y === 0.U || y === 19.U) {
-        //dataMemory.io.dataWrite := 0.U(32.W)
-        //dataMemory.io.address := addressReg
+        io.dataWrite := 0.U(32.W)
         dataReg := 0.U(32.W)
         stateReg := write
       }
       when((x > 0.U && x < 19.U) && (y > 0.U && y < 19.U)) {
         trueAddressReg := addressReg //How memory works forces us to set a true Address Register
-        addressReg := trueAddressReg-1.U(16.W) // Prepping the left neighbor
-        dataReg := Cat(0.U(24.W), dataMemory.io.dataRead(7,0))
+        addressReg := addressReg - 1.U(16.W) // Prepping the left neighbor
+        io.address := addressReg - 1.U(16.W)
+        dataReg := Cat(0.U(24.W), io.dataRead(7,0))
         stateReg := self
       }
     }
     is(self) {
       when(dataReg === 0.U(32.W)){
-        stateReg := write
+        io.dataWrite := 0.U(32.W)
         addressReg := trueAddressReg
+        stateReg := write
       }
       when(dataReg === 255.U(32.W)){
         addressReg := trueAddressReg+1.U(16.W) //Preparing the coordinate for the right neighbor
-        dataReg := Cat(0.U(24.W), dataMemory.io.dataRead(7,0))
+        io.address := trueAddressReg+1.U(16.W)
+        dataReg := Cat(0.U(24.W), io.dataRead(7,0))
         stateReg := left
       }
     }
     is(left){
       when(dataReg === 0.U(32.W)){
-        stateReg := write
+        io.dataWrite := 0.U(32.W)
         addressReg := trueAddressReg
+        stateReg := write
       }
       when(dataReg === 255.U(32.W)){
         addressReg := trueAddressReg-20.U(16.W) // Preparing the coordinate for up
-        dataReg := Cat(0.U(24.W), dataMemory.io.dataRead(7,0))
+        io.address := trueAddressReg-20.U(16.W)
+        dataReg := Cat(0.U(24.W), io.dataRead(7,0))
         stateReg := right
       }
     }
     is(right){
-      when(dataReg === 0.U(16.W)){
-      stateReg := write
-      addressReg := trueAddressReg
+      when(dataReg === 0.U(32.W)){
+        addressReg := trueAddressReg
+        stateReg := write
       }
-      when(dataReg === 255.U(16.W)){
+      when(dataReg === 255.U(32.W)){
         addressReg := trueAddressReg+20.U(16.W)
-        dataReg := Cat(0.U(24.W), dataMemory.io.dataRead(7,0))
+        io.address := trueAddressReg+20.U(16.W)
+        dataReg := Cat(0.U(24.W), io.dataRead(7,0))
         stateReg := up
       }
     }
     is(up){
-      when(dataReg === 0.U(16.W)){
-        stateReg := write
+      when(dataReg === 0.U(32.W)){
         addressReg := trueAddressReg
+        stateReg := write
       }
-      when(dataReg === 255.U(16.W)){
-        dataReg := Cat(0.U(24.W), dataMemory.io.dataRead(7,0))
+      when(dataReg === 255.U(32.W)){
+        addressReg := trueAddressReg+20.U(16.W)
+        io.address := trueAddressReg+20.U(16.W)
+        dataReg := Cat(0.U(24.W), io.dataRead(7,0))
         stateReg := down
       }
     }
     is(down){
-      when(dataReg === 0.U(16.W)){
+      when(dataReg === 0.U(32.W)){
         addressReg := trueAddressReg
-        dataReg := 0.U(32.W)
+        io.dataWrite := 0.U(32.W)
         stateReg := write
       }
-      when(dataReg === 255.U(16.W)){
-        addressReg := trueAddressReg /// White value
+      when(dataReg === 255.U(32.W)){
+        io.address := trueAddressReg + 400.U(16.W)
+        io.dataWrite := 255.U(32.W) /// White value
         stateReg := write
       }
     }
 
 
     is(write) {
-      dataMemory.io.address := addressReg + 400.U(16.W)
-      dataMemory.io.writeEnable := true.B
+      io.address := trueAddressReg + 400.U(16.W)
+      io.writeEnable := true.B
       when(addressReg === 399.U(16.W)){
         stateReg := done
       }.otherwise{
@@ -126,7 +132,7 @@ class Accelerator extends Module {
       }
     }
     is(inc){
-      dataMemory.io.writeEnable := false.B
+      io.writeEnable := false.B
       when(x === 19.U){
         x := 0.U
         y := y+1.U
